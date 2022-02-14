84f2d5b76f9f569ebf3446df2dc940aa7cdd5037
misha2212 <88970804+misha2212@users.noreply.github.com>
2022-02-14 19:03:01 +0700

    add via upload

---
+++ task.py
@@ -0,0 +1,50 @@
+from dadata import Dadata
+import sqlite3
+
+name = input('Введите Ваше имя: ')
+token = input('Введите Ваш API ключ: ')
+choise_language = input('Для ввода запросов и вывода результатов поддерживаются языки: английский/русский.'
+                        ' Оставить русский язык по умолчанию? Введите y/n : ')
+if choise_language == 'y':
+    language = 'ru'
+elif choise_language == 'n':
+    language = 'en'
+
+
+def users_data(name, token, language):
+    with sqlite3.connect("database.db") as sqlite_connection:
+        cursor_db = sqlite_connection.cursor()
+        cursor_db.execute("""CREATE TABLE IF NOT EXISTS users (
+                            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name TEXT NOT NULL,
+                            api_key TEXT NOT NULL,
+                            language TEXT NOT NULL 
+                             )""")
+        insert_data = """INSERT INTO users(name, api_key, language) VALUES(?, ?, ?)"""
+        data_tuple = (name, token, language)
+        cursor_db.execute(insert_data, data_tuple)
+
+users_data(name, token, language)
+
+
+def search_geo():
+    while True:
+        user_continue = input('Здесь Вы можете узнать координаты введенного адреса. Продолжить?'
+                              ' Введите y / n : ')
+        if user_continue == 'y':
+            dadata = Dadata(token)
+            user_address = input('Введите желаемый адрес: ')
+            result = dadata.suggest("address", user_address, language=language)
+            punkt = 0
+            for i in result:
+                punkt += 1
+                print(str(punkt) + ': ' + i['value'])
+            user_choice = int(input('Введите порядковый номер Вашего объекта: '))
+            user_dict = result[user_choice - 1]
+            latitude = user_dict['data']['geo_lat']
+            longitude = user_dict['data']['geo_lon']
+            print('Координаты Вашего объекта: широта: ' + latitude + ' , долгота: ' + longitude)
+        elif user_continue == 'n':
+            break
+
+search_geo()
